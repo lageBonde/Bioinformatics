30c553bbdf35aabc088569d8c0c0399a8ba0e809
lageBonde <102809329+lageBonde@users.noreply.github.com>
2026-02-01 12:49:38 +0000

    .

--- convolution-gc.py
+++ convolution-gc.py
@@ -1,5 +1,8 @@
 from Bio import SeqIO
-from numpy import convolve
+import numpy as np
+import scipy.stats
+import matplotlib.pyplot as plt
+import sys
 
 def base_to_gc_percentage(base):
     if base.lower() == "g" or base.lower() == "c":
@@ -7,11 +10,30 @@ def base_to_gc_percentage(base):
     return 0
 
 records = [record for record in SeqIO.parse("/workspaces/Bioinformatics/genomes/GCF_024807035.1_ASM2480703v1_genomic.fna", "fasta")]
-print(records[0].seq[0:10])
 
-convolve(
-    [base_to_gc_percentage(base) for base in records[0].seq],
-    [0,]# normal dist...........
+range_ = 1000
+mu = range_/2
+sigma = 500
+
+normal_dist = scipy.stats.norm.pdf(
+    np.array([i for i in range(range_)]),
+    mu,
+    sigma
 )
 
-# matplot
+plt.text(0,1.05, 'GCF_024807035.1_ASM2480703v1_genomic, record '+sys.argv[1], transform=plt.gca().transAxes)
+
+plt.plot(
+    np.convolve(
+        [base_to_gc_percentage(base) for base in records[
+            int(sys.argv[1])
+        ].seq],
+        normal_dist,
+        mode="valid"
+    )
+)
+plt.xlabel("bp", fontsize="15")
+plt.ylabel("%GC", fontsize="15")
+
+plt.show()
+plt.savefig("Convolution")
