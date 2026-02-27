5c45b1fb2d81decfb643d5a0f1da9063701f41a6
lageBonde <102809329+lageBonde@users.noreply.github.com>
2026-02-27 13:30:01 +0000

    Search by tata box

---
+++ compare_genomes/searching_for_tata_box.py
@@ -0,0 +1,35 @@
+from Bio import SeqIO
+from Bio.Seq import Seq
+from Bio import Align
+import numpy as np
+import math, re
+import matplotlib.pyplot as plt
+
+seq_16790_recs = [rec.seq for rec in SeqIO.parse("/workspaces/Bioinformatics/compare_genomes/GCA_000009185.1_ASM918v1_genomic.fna", "fasta")]
+seq_D2T01_recs = [rec.seq for rec in SeqIO.parse("/workspaces/Bioinformatics/compare_genomes/GCA_903989505.1_D2T2_genomic.fna", "fasta")]
+
+def splitByTATABoxes(seq):
+    seq_str = str(seq)
+    motif = "TATA[AT]A[AT]"
+
+    return re.split(motif,seq_str)
+
+def main():
+    genes = splitByTATABoxes(seq_16790_recs[0])
+
+    aligner = Align.PairwiseAligner()
+
+    results = []
+    done = 0
+    for gene in genes:
+        try:
+            results.append(
+                aligner.score(seq_D2T01_recs,gene)
+            )
+        except ValueError:
+            pass
+        print(done/len(genes), "%", end="\r")
+    plt.plot(len(results),results)
+    plt.savefig("bysearch_tata")
+
+main()
